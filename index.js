f78807320c4214e2309837a52b5c5c95dcb3cd31
FlozyTess <cecilrency23@gmail.com>
2024-12-01 06:10:17 +0300

    did the modulo and the random integer

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ let num2 = 2;
 let multiply = num1 * num2;
 
 console.log(multiply);  // Output will be 62
+let random = Math.floor(Math.random() * 100) + 1;  
+
+console.log(random);  // Output will be a random integer greater than 0
+let num3 = 24;
+let num4 =20;
+let mod = num3 % num4;
+console.log(mod); //Output will be remainder 4
+
