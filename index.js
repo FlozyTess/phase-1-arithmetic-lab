abd39ba86c0951fffae704217fe96f5c7f4f31df
FlozyTess <cecilrency23@gmail.com>
2024-12-01 06:16:06 +0300

    completed lab

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ let num2 = 2;
 let multiply = num1 * num2;
 
 console.log(multiply);  // Output will be 62
+
 let random = Math.floor(Math.random() * 100) + 1;  
 
 console.log(random);  // Output will be a random integer greater than 0
 let num3 = 24;
 let num4 =20;
 let mod = num3 % num4;
+
 console.log(mod); //Output will be remainder 4
+let num5 = 5;
+let num6 = 8;
+let num7 = 15;
+let num8 = 20;
+let max = Math.max(num5, num6, num7, num8);
+
+console.log(max);  // Output will be 20
 
